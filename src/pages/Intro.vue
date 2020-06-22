33c7ac080856c0bb0665c9d567aa0e205142b4b0
Thuong Hoai <thuong.le@hongkyfengshui.com>
2020-06-22 14:45:19 +0700

    deploy

---
+++ src/pages/Intro.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bg-main introContent">
+    <q-carousel
+      animated
+      v-model="slide"
+      swipeable
+      navigation
+      infinite
+    >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg"></q-carousel-slide>
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg"></q-carousel-slide>
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg"></q-carousel-slide>
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg"></q-carousel-slide>
+    </q-carousel>
+    <div class="introFooter">
+        <q-btn class="btnStart" @click.prevent="goToLoveTree" label="Bắt đầu" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Intro',
+  data () {
+    return {
+      slide: 1
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+    goToLoveTree () {
+      if (window.ReactNativeWebView) {
+        window.ReactNativeWebView.postMessage('LOVETREE')
+      }
+    }
+  }
+}
+</script>
